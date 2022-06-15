5f70647c07a5e69a8df55e9ec87acf7df8ceccab
Jörg Thalheim <joerg@thalheim.io>
2022-06-15 23:53:21 +0200

    use legacyPackages instead of inventing a new flake output

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
   description = "nix-index database";
   outputs = _:
     {
-      database.x86_64-linux = builtins.fetchurl {
+      legacyPackages.x86_64-linux.database = builtins.fetchurl {
         url = "https://github.com/Mic92/nix-index-database/releases/download/2022-06-12/index-x86_64-linux";
         sha256 = "09l4h2pz2lq5axgdj31kpymnvsrgfwsccdfqm4ycmg7jl1kc4qn6";
       };
-      database.x86_64-darwin = builtins.fetchurl {
+      legacyPackages.x86_64-darwin.database = builtins.fetchurl {
         url = "https://github.com/Mic92/nix-index-database/releases/download/2022-06-12/index-x86_64-darwin";
         sha256 = "0r7kjpcgg679r6m0s0g1imsd0xi1p6x7f42hv5l2kwl679rsrziv";
       };
