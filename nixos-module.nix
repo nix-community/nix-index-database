2089565c0ef92510d4c8ef7e88a70b31f5ffd544
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-04-14 00:23:16 +0200

    Use pkgs.stdenv.system

--- nixos-module.nix
+++ nixos-module.nix
@@ -1,5 +1,5 @@
 { packages }:
 { pkgs, ... }: {
   programs.nix-index.enable = true;
-  programs.nix-index.package = packages.${pkgs.system}.nix-index-with-db;
+  programs.nix-index.package = packages.${pkgs.stdenv.system}.nix-index-with-db;
 }
