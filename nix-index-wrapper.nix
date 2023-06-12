2ebac51dd21aa69ded74eabebabb37d2f157851b
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-06-12 17:39:06 +0200

    Use NIX_INDEX_DATABASE instead of XDG_CACHE_HOME
    Should fix #48.

--- nix-index-wrapper.nix
+++ nix-index-wrapper.nix
@@ -11,7 +11,7 @@ runCommand "nix-index-with-db-${nix-index-unwrapped.version}"
   mkdir -p $out/share/cache/nix-index
   ln -s ${nix-index-database} $out/share/cache/nix-index/files
   makeWrapper ${nix-index-unwrapped}/bin/nix-locate $out/bin/nix-locate \
-    --set XDG_CACHE_HOME $out/share/cache
+    --set NIX_INDEX_DATABASE $out/share/cache/nix-index
 
   mkdir --parents $out/etc/profile.d
   substitute \
