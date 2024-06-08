41d679cdb2320cefdee6b0f6c8add33daa38ee9a
Gerg-L <GregLeyda@proton.me>
2024-06-08 19:09:58 -0400

    wrappers: rework to use symlinkJoin
    makeWrapper -> makeBinaryWrapper
    
    use nix "if else" instead of bash "if else"

--- comma-wrapper.nix
+++ comma-wrapper.nix
@@ -1,36 +1,36 @@
-{ runCommand
-, makeWrapper
+{ lib
+, symlinkJoin
+, makeBinaryWrapper
 , comma
 , nix-index-unwrapped
 , nix-index-database
+,
 }:
 let
   commaOverridden = comma.override { inherit nix-index-unwrapped; };
 in
-runCommand "comma-with-db-${comma.version}"
-{
-  nativeBuildInputs = [ makeWrapper ];
+symlinkJoin {
+  name = "comma-with-db-${comma.version}";
+  paths = [ commaOverridden ];
+  nativeBuildInputs = [ makeBinaryWrapper ];
+  postBuild = ''
+    mkdir -p $out/share/cache/nix-index
+    ln -s ${nix-index-database} $out/share/cache/nix-index/files
+
+    for cmd in "," "comma"; do
+      ${if lib.versionAtLeast nix-index-unwrapped.version "0.1.6" then
+      ''
+        wrapProgram "$out/bin/$cmd" \
+          --set NIX_INDEX_DATABASE $out/share/cache/nix-index
+      ''
+      else
+      ''
+        wrapProgram "$out/bin/$cmd" \
+          --set XDG_CACHE_HOME $out/share/cache
+      ''
+      }
+    done
+  '';
+
   meta.mainProgram = "comma";
-} ''
-  # Function that does the same as nixpkgs.lib.versionAtLeast,
-  # return true if the first string denotes a version equal to or newer than
-  # the second one.
-  function versionAtLeast() {
-    printf '%s\n' "$2" "$1" | sort --version-sort --check=quiet
-  }
-
-  mkdir -p $out/share/cache/nix-index
-  ln -s ${nix-index-database} $out/share/cache/nix-index/files
-
-  version="$(${nix-index-unwrapped}/bin/nix-index --version | cut -d' ' -f2)"
-
-  for cmd in "," "comma"; do
-    if versionAtLeast "''${version}" "0.1.6"; then
-      makeWrapper ${commaOverridden}/bin/''${cmd} $out/bin/''${cmd} \
-        --set NIX_INDEX_DATABASE $out/share/cache/nix-index
-    else
-      makeWrapper ${commaOverridden}/bin/''${cmd} $out/bin/''${cmd} \
-        --set XDG_CACHE_HOME $out/share/cache
-    fi
-  done
-''
+}
--- nix-index-wrapper.nix
+++ nix-index-wrapper.nix
@@ -1,36 +1,30 @@
-{ runCommand
-, makeWrapper
+{ lib
+, symlinkJoin
+, makeBinaryWrapper
 , nix-index-unwrapped
 , nix-index-database
+,
 }:
-runCommand "nix-index-with-db-${nix-index-unwrapped.version}"
-{
-  nativeBuildInputs = [ makeWrapper ];
+symlinkJoin {
+  name = "nix-index-with-db-${nix-index-unwrapped.version}";
+  paths = [ nix-index-unwrapped ];
+  nativeBuildInputs = [ makeBinaryWrapper ];
+  postBuild = ''
+    mkdir -p $out/share/cache/nix-index
+    ln -s ${nix-index-database} $out/share/cache/nix-index/files
+
+    ${if lib.versionAtLeast nix-index-unwrapped.version "0.1.6" then
+    ''
+      wrapProgram $out/bin/nix-locate \
+        --set NIX_INDEX_DATABASE $out/share/cache/nix-index
+    ''
+    else
+    ''
+      wrapProgram $out/bin/nix-locate \
+        --set XDG_CACHE_HOME $out/share/cache
+    ''
+    }
+  '';
+
   meta.mainProgram = "nix-locate";
-} ''
-  # Function that does the same as nixpkgs.lib.versionAtLeast,
-  # return true if the first string denotes a version equal to or newer than
-  # the second one.
-  function versionAtLeast() {
-    printf '%s\n' "$2" "$1" | sort --version-sort --check=quiet
-  }
-
-  mkdir -p $out/share/cache/nix-index
-  ln -s ${nix-index-database} $out/share/cache/nix-index/files
-
-  version="$(${nix-index-unwrapped}/bin/nix-index --version | cut -d' ' -f2)"
-
-  if versionAtLeast "''${version}" "0.1.6"; then
-    makeWrapper ${nix-index-unwrapped}/bin/nix-locate $out/bin/nix-locate \
-      --set NIX_INDEX_DATABASE $out/share/cache/nix-index
-  else
-    makeWrapper ${nix-index-unwrapped}/bin/nix-locate $out/bin/nix-locate \
-      --set XDG_CACHE_HOME $out/share/cache
-  fi
-
-  mkdir --parents $out/etc/profile.d
-  substitute \
-    "${nix-index-unwrapped}/etc/profile.d/command-not-found.sh" \
-    "$out/etc/profile.d/command-not-found.sh" \
-    --replace "${nix-index-unwrapped}" "$out"
-''
+}
