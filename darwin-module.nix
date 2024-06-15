d63f03b4d3a85d52173c3d49690e8afe3b29110d
Eyad Sibai <eyadsibai@users.noreply.github.com>
2024-06-15 20:49:55 +0300

    nixfmt formatting

--- darwin-module.nix
+++ darwin-module.nix
@@ -1,18 +1,19 @@
 self:
-{ pkgs, lib, ... }:
-{
+{ pkgs, lib, ... }: {
   programs.nix-index = {
     enable = lib.mkDefault true;
-    package = lib.mkDefault self.packages.${pkgs.stdenv.system}.nix-index-with-db;
+    package =
+      lib.mkDefault self.packages.${pkgs.stdenv.system}.nix-index-with-db;
   };
 
-options = {
-    programs.nix-index-database.comma.enable = lib.mkEnableOption "wrapping comma with nix-index-database and put it in the PATH";
+  options = {
+    programs.nix-index-database.comma.enable = lib.mkEnableOption
+      "wrapping comma with nix-index-database and put it in the PATH";
   };
 
-environment.systemPackages =
-      lib.optional config.programs.nix-index-database.comma.enable
-        self.packages.${pkgs.stdenv.system}.comma-with-db;
+  environment.systemPackages =
+    lib.optional config.programs.nix-index-database.comma.enable
+    self.packages.${pkgs.stdenv.system}.comma-with-db;
 
   _file = ./darwin-module.nix;
 }
