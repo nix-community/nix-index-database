8121c0448add92ae974468e441871759a7ac012e
Eyad Sibai <eyadsibai@users.noreply.github.com>
2024-06-15 20:45:29 +0300

    Add comma option to darwin module

--- darwin-module.nix
+++ darwin-module.nix
@@ -1,9 +1,23 @@
 self:
-{ pkgs, lib, ... }:
 {
-  programs.nix-index = {
-    enable = lib.mkDefault true;
-    package = lib.mkDefault self.packages.${pkgs.stdenv.system}.nix-index-with-db;
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options = {
+    programs.nix-index-database.comma.enable = lib.mkEnableOption "wrapping comma with nix-index-database and put it in the PATH";
+  };
+  config = {
+    programs.nix-index = {
+      enable = lib.mkDefault true;
+      package = lib.mkDefault self.packages.${pkgs.stdenv.system}.nix-index-with-db;
+    };
+
+    environment.systemPackages = lib.mkIf config.programs.nix-index-database.comma.enable [
+      self.packages.${pkgs.stdenv.system}.comma-with-db
+    ];
   };
 
   _file = ./darwin-module.nix;
