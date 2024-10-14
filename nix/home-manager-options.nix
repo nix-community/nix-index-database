fd5beeebf9531b27468bdea9fd2590967f2e9e91
Jörg Thalheim <joerg@thalheim.io>
2024-10-14 08:35:22 +0200

    more defensivly seperate options from configuration
    we still have to have cases where imported the module twice succeeds:
    https://github.com/nix-community/nix-index-database/issues/125
    
    by having avoiding anonymous modules with options, this is hopefully
    resolved.

---
+++ nix/home-manager-options.nix
@@ -0,0 +1,12 @@
+{ lib, config, ... }: {
+  options = {
+    programs.nix-index.symlinkToCacheHome = lib.mkOption {
+      type = lib.types.bool;
+      default = config.programs.nix-index.enable;
+      description = ''
+        Whether to symlink the prebuilt nix-index database to the default
+        location used by nix-index. Useful for tools like comma.
+      '';
+    };
+  };
+}
--- nixos-module.nix
+++ nixos-module.nix
@@ -1,25 +1,18 @@
 self:
 {
-  config,
-  pkgs,
   lib,
+  pkgs,
+  config,
   ...
 }:
 {
-  options = {
-    programs.nix-index-database.comma.enable = lib.mkEnableOption "wrapping comma with nix-index-database and put it in the PATH";
-  };
+  imports = [ ./nix/shared.nix ];
 
-  config = {
-    programs.nix-index = {
-      enable = lib.mkDefault true;
-      package = lib.mkDefault self.packages.${pkgs.stdenv.system}.nix-index-with-db;
-    };
-    programs.command-not-found.enable = lib.mkDefault false;
-    environment.systemPackages =
-      lib.optional config.programs.nix-index-database.comma.enable
-        self.packages.${pkgs.stdenv.system}.comma-with-db;
-  };
+  programs.nix-index.package = lib.mkDefault self.packages.${pkgs.stdenv.system}.nix-index-with-db;
+
+  programs.command-not-found.enable = lib.mkDefault false;
+  environment.systemPackages = lib.mkIf config.programs.nix-index-database.comma.enable
+    [ self.packages.${pkgs.stdenv.system}.comma-with-db ];
 
   _file = ./nixos-module.nix;
 }
