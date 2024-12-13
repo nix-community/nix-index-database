e35a2c27f8ebf0f4bb5e9bd46e394c2e9c56bd84
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-12-13 11:28:26 +0100

    shared: add programs.nix-index.acknowledgeBreakingChange option
    Link: https://github.com/nix-community/nix-index-database/pull/132

--- nix/shared.nix
+++ nix/shared.nix
@@ -1,5 +1,22 @@
-{ lib, ... }: {
+{ config, lib, ... }: {
   options = {
-    programs.nix-index-database.comma.enable = lib.mkEnableOption "wrapping comma with nix-index-database and put it in the PATH";
+    programs = {
+      nix-index-database.comma.enable = lib.mkEnableOption "wrapping comma with nix-index-database and put it in the PATH";
+
+      # TODO: Remove this option after the release of nixos-25.11.
+      nix-index.acknowledgeBreakingChange = lib.mkOption {
+        default = false;
+        example = true;
+        description = ''Whether to acknowledge the breaking change introduced in PR https://github.com/nix-community/nix-index-database/pull/132 ("treewide: disable programs.nix-index.enable option by default").'';
+        type = lib.types.bool;
+      };
+    };
   };
+
+  config.programs.nix-index.enable = lib.mkDefault (
+    lib.warnIfNot
+    config.programs.nix-index.acknowledgeBreakingChange
+    "nix-index-database: programs.nix-index.enable no longer defaults to true. To disable this warning, set 'programs.nix-index.acknowledgeBreakingChange = true;' or override programs.nix-index.enable."
+    false
+  );
 }
