92d47bb2053c7185ba51523b9409ead244b4e7a4
Gavin John <gavinnjohn@gmail.com>
2025-07-18 10:22:46 -0400

    comma: Use overrideAttrs instead of creating a new derivation

--- comma-wrapper.nix
+++ comma-wrapper.nix
@@ -1,25 +1,26 @@
 {
   linkFarm,
-  symlinkJoin,
   makeBinaryWrapper,
   comma,
   nix-index-unwrapped,
   nix-index-database,
 }:
-let
-  commaOverridden = comma.override { inherit nix-index-unwrapped; };
-in
-symlinkJoin {
-  name = "comma-with-db-${comma.version}";
-  paths = [ commaOverridden ];
-  nativeBuildInputs = [ makeBinaryWrapper ];
-  databaseDirectory = linkFarm "nix-index-database" { files = nix-index-database; };
-  postBuild = ''
-    for cmd in "," "comma"; do
-      wrapProgram "$out/bin/$cmd" \
-        --set NIX_INDEX_DATABASE $databaseDirectory
-    done
-  '';
 
-  meta.mainProgram = "comma";
-}
+(comma.override {
+  inherit nix-index-unwrapped;
+}).overrideAttrs
+  (oldAttrs: {
+    pname = "comma-with-db";
+
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ makeBinaryWrapper ];
+
+    databaseDirectory = linkFarm "nix-index-database" { files = nix-index-database; };
+
+    postInstall =
+      (oldAttrs.postInstall or "")
+      + ''
+        for cmd in "," "comma"; do
+          wrapProgram "$out/bin/$cmd" --set NIX_INDEX_DATABASE $databaseDirectory
+        done
+      '';
+  })
