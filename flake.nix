a95d227778c3255cd16907160fa9b48eb914f466
Giel van Schijndel <giel@mortis.eu>
2026-03-31 11:52:52 +0200

    feat: make 'comma' package configurable
    To, for example, allow having only a single database. This helps to
    minimize fetches and thus avoid needing them when modifying configs on
    systems without internet.

--- flake.nix
+++ flake.nix
@@ -64,6 +64,7 @@
         import ./tests.nix {
           inherit system nixpkgs;
           nixIndexModule = self.nixosModules.nix-index;
+          nixIndexOverlay = self.overlays.nix-index;
         }
       );
 
--- nixos-module.nix
+++ nixos-module.nix
@@ -6,13 +6,14 @@
 }:
 let
   packages = import ./. { inherit pkgs; };
+  cfg = config.programs.nix-index-database;
 in
 {
   imports = [ ./nix/shared.nix ];
 
   programs.nix-index.package = lib.mkDefault packages.nix-index-with-db;
   programs.command-not-found.enable = lib.mkDefault false;
-  environment.systemPackages = lib.mkIf config.programs.nix-index-database.comma.enable [
-    packages.comma-with-db
+  environment.systemPackages = lib.mkIf cfg.comma.enable [
+    cfg.comma.package
   ];
 }
